187dba7aa16890c0a9e5996126525a8771ad1140
natalysiyah <natasha.artyuh@gmail.com>
2019-10-06 14:53:28 +0300

    Index ready

--- routes/Home.js
+++ routes/Home.js
@@ -6,8 +6,10 @@ const path = require("path");
 router.get("/", (req,res) => {
      res.render("index", {
         title: "Node Pug",
-        h1: "Hello pug!",
-        name: "Bill"
+        h1: ["best online", "Learning system"],
+        h4: "Get started with online courses",
+        p: ["Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium","Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour."],
+        h2: ["Learn From The Experts","Book Library \&amp\; Store","Best Course Online","Best Industry Leaders"]
     });
 })
 
